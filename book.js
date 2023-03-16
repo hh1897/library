ecaae52b4cf75edb42995dc9ed1078c408e59447
hh1897 <64330680+hh1897@users.noreply.github.com>
2023-03-16 19:41:33 +0000

    Create book object

---
+++ book.js
@@ -0,0 +1,23 @@
+// create book
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+
+    this.info = function() {
+        let sentence = title + " by " + author + ", " + pages + " pages, ";
+        let hasRead = "";
+        if (read == true) {
+            hasRead = "has been read.";
+        }
+        else {
+            hasRead = "not read yet.";
+        }
+        sentence = sentence + hasRead;
+
+        return sentence;
+
+    }
+}
