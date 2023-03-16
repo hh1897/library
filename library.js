d8e8fb505932f6b9ca0ea2cfc0fc88accfbc8753
hh1897 <64330680+hh1897@users.noreply.github.com>
2023-03-16 20:36:27 +0000

    added new function
    new function to add book to library

---
+++ library.js
@@ -0,0 +1,29 @@
+let myLibrary = [];
+
+// Book constructor
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+
+    this.info = function() {
+        let sentence = title + " by " + author + ", " + pages + " pages, ";
+        let hasRead = "";
+        if (read == true) {
+            hasRead = "has been read.";
+        }
+        else {
+            hasRead = "not read yet.";
+        }
+        sentence = sentence + hasRead;
+
+        return sentence;
+
+    }
+}
+
+function addBookToLibrary(book) {
+    myLibrary.push(book);
+}
+
