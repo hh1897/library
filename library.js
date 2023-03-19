617d6d7ce6f039fde1823921c2c7c349c5854b76
hh1897 <64330680+hh1897@users.noreply.github.com>
2023-03-19 15:43:11 +0000

    addBookForm function
    added basic form through javascript DOM

--- library.js
+++ library.js
@@ -40,6 +40,60 @@ function displayBooks() {
     }
 }
 
+function addBookForm() {
+    // Create form 
+    let form = document.createElement("FORM");
+    form.id = "bookForm";
+    
+    // Create title input
+    let title = document.createElement("label");
+    title.id = "title";
+    title.innerHTML = "Title: ";
+    
+    let titleInput = document.createElement("input");
+    titleInput.type = "text";
+    title.appendChild(titleInput);
+    form.appendChild(title);
+
+    // Author input
+    let author = document.createElement("label");
+    author.id = "author";
+    author.innerHTML = "Author: ";
+    
+    let authorInput = document.createElement("input");
+    authorInput.type = "text";
+    author.appendChild(authorInput);
+    form.appendChild(author);
+    
+    // Pages input
+    let pages = document.createElement("label");
+    pages.id = "pages";
+    pages.innerHTML = "Pages: ";
+    
+    let pagesInput = document.createElement("input");
+    pagesInput.type = "number";
+    pages.appendChild(pagesInput);
+    form.appendChild(pages);
+
+    // Read input
+    let read = document.createElement("label");
+    read.id = "read";
+    read.innerHTML = "Read: ";
+    
+    let readInput = document.createElement("input");
+    readInput.type = "checkbox";
+    read.appendChild(readInput);
+    form.appendChild(read);
+
+    // Append form to existing HTML div at the end 
+    let parent = document.getElementById("formParent");
+    parent.appendChild(form);
+}
+
+
+
+
+
 let theHobbit = new Book("the hobbit", "JRRTolkein", 300, false);
 addBookToLibrary(theHobbit);
 let daVinciCode = new Book("da vinci code", "dan brown", 100, true);
